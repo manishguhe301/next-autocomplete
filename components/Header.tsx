9e90c540b3fab380f2584f904e85b8b35bc4450c
Manish <manishguhe301@gmail.com>
2025-03-27 18:16:15 +0530

    feat: header for the Application

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='text-xl font-semibold text-gray-900 bg-white p-4 border-b border-gray-300 shadow-sm text-center'>
+      React Autocomplete (Combo box) with Arrow Navigation
+    </div>
+  );
+};
+
+export default Header;
