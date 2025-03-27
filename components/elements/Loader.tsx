647c1a0c2093eb9342ee5a87a58b2971305fb066
Manish <manishguhe301@gmail.com>
2025-03-27 18:53:33 +0530

    feat: installed the icons and loader

---
+++ components/elements/Loader.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className='container'>
+      <div className='dot'></div>
+      <div className='dot'></div>
+      <div className='dot'></div>
+      <div className='dot'></div>
+      <div className='dot'></div>
+      <div className='dot'></div>
+    </div>
+  );
+};
+
+export default Loader;
